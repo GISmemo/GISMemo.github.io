751f9d3c370d8b75a3b51c8c5e099ef37c10e02e
Yu Chun Tsao <tsao84672776@gmail.com>
2022-05-25 17:00:42 +0800

    fixed ImageModal mobile css and hidden overflow

--- components/ImageModal.js
+++ components/ImageModal.js
@@ -1,5 +1,5 @@
 const ImageModal = (props) => {
-  props.rest.width = window.innerWidth * 0.9
+  props.rest.width = window.innerWidth * 0.8
 
   const imageClick = (e) => {
     e.stopPropagation()
@@ -9,14 +9,14 @@ const ImageModal = (props) => {
     <div>
       <div className="relative z-10">
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        <div className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="fixed inset-0 z-10 overflow-hidden">
           <div
             onClick={props.onClose}
-            className="flex min-h-full items-end justify-center p-4 sm:items-center sm:p-0"
+            className="flex min-h-full place-items-center justify-center p-4 sm:items-center sm:p-0"
           >
             <div className="relative">
-              <img onClick={imageClick} {...props.rest} />
-              <div className="modal-close absolute top-10 right-5 z-50 mt-4 mr-4 flex cursor-pointer flex-col items-center rounded-full bg-gray-100 p-2">
+              <img className="m-0" onClick={imageClick} {...props.rest} />
+              <div className="modal-close absolute top-5 right-5 z-50 flex cursor-pointer flex-col items-center rounded-full bg-gray-100 p-2">
                 <svg
                   className="fill-current text-black"
                   xmlns="http://www.w3.org/2000/svg"
